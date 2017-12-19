b51bd7cdf57a536a6e5e6932fc0f3c571fa6a42e
Jay Ryan <jay.ryan@ge.com>
2017-12-19 13:22:38 -0500

    update workstation bootstrap scripts

--- basic.rb
+++ basic.rb
@@ -1,9 +1,9 @@
 chocolatey_package 'git' do
   package_name 'git'
-  options '-params /GitAndUnixToolsOnPath'
+  options '--params /GitAndUnixToolsOnPath /WindowsTerminal'
 end
 
-packages =  %w[conemu visualstudiocode 7zip winmerge vagrant autohotkey]
+packages =  %w[Git-Credential-Manager-for-Windows putty visualstudiocode mremoteNG 7zip winmerge autohotkey conemu virtualbox vagrant python2 python3 docker-for-windows chrome firefox WindowsAzurePowershell dbeaver sysinternals]
 packages.each do |pkg|
   chocolatey_package pkg
 end
@@ -15,31 +15,24 @@ gems.each do |gem|
   end
 end
 
-extensions = %w[ ms-vscode.PowerShell msazurermtools.azurerm-vscode-tools Pendrica.Chef ]
+extensions = %w[ ms-vscode.PowerShell bbenoist.vagrant marcostazi.vs-code-vagrantfile msazurermtools.azurerm-vscode-tools Pendrica.Chef PeterJausovec.vscode-docker tariky.easy-snippet-maker GrapeCity.gc-excelviewer eamodio.gitlens secanis.jenkinsfile-support DavidAnson.vscode-markdownlint ms-python.python Shan.code-settings-sync]
 extensions.each do |ext|
   execute "install vscode extension #{ext}" do
     command "code --install-extension #{ext}"
   end
 end
 
-powershell_script 'Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force' do
-end
-powershell_script 'Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart' do
+powershell_script 'name' do
+  code <<-EOH
+  Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+  Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart
+  Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+  Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V,Microsoft-Winodws-Subsystem-Linux -All -NoRestart
+  EOH
 end
 
-
-modules = %w[ PSReadline ]
+modules = %w[ PSReadline Carbon psake posh-git ImportExcel posh-docker AWSPowerShell Plaster platyPS SHiPS Invoke-SqlCmd2 ]
 modules.each do |mod|
   powershell_script "Install-Module #{mod} -Force" do
   end
-end
-
-
-directory 'C:\Work\Chef' do
-  recursive true
-  action :create
-end
-
-execute 'chef generate repo example_repo -p' do
-  cwd 'C:\Work\Chef'
 end
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,14 +1,19 @@
 # Setup my execution policy for both the 64 bit and 32 bit shells
 set-executionpolicy remotesigned
+
+#---- TEMPORARY ---
+Disable-UAC
+
 start-job -runas32 {set-executionpolicy remotesigned} | receive-job -wait
 # Install fixed version of ChefDK 
 invoke-restmethod 'https://omnitruck.chef.io/install.ps1' | Invoke-Expression
 install-project chefdk -verbose -version 1.1.16
 # Install Chocolatey
 invoke-expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+. { Invoke-WebRequest -UseBasicParsing http://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; get-boxstarter -Force
 choco feature enable -n allowGlobalConfirmation
 # Get a basic setup recipe
-Invoke-RestMethod -UseBasicParsing 'https://gist.githubusercontent.com/karstenmueller/0c2257684b8e6a44bac9526c52074e18/raw/a150048b7ffbfe202f90fdce446b8095b3e7858c/basic.rb' | out-file -encoding ascii -filepath c:/basic.rb
+Invoke-RestMethod -UseBasicParsing 'https://gist.githubusercontent.com/jaywryan/d2207f41a3c3543f39045b9a0e489250/raw/e9d39ae43dcc7e18fb2a33aa02c7928e4f3c018f/basic.rb' | out-file -encoding ascii -filepath $env:TEMP/basic.rb
 # We dont need any Windows 10 Apps
 # Get-AppxPackage | Remove-AppxPackage 2>&1>$null
 # Use Chef Apply to setup 
