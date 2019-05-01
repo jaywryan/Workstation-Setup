36fcae78e583521986309f36c7e9c0c296e53065
Jay Ryan <jay.ryan@ge.com>
2019-04-30 23:17:21 -0400

    adding mac install info

---
+++ mac-setup.sh
@@ -0,0 +1,6 @@
+
+brew cask install iterm2 visual-studio-code git spectacle slack docker powershell java
+sudo chown -R $(whoami) /usr/local/lib/pkgconfig
+brew install tmux git git-credential-manager
+git config --global user.email "jay.ryan@ge.com"
+git config --global user.name "Jay Ryan"
