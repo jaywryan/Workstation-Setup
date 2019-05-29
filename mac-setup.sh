b8cd6c1ad16a8514ec4a794caf58e72873252a75
Jay Ryan <jay.ryan@ge.com>
2019-05-29 17:07:12 -0400

    added defaults and powerline install

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,6 +1,10 @@
-
 brew cask install iterm2 visual-studio-code git spectacle slack docker powershell java
 sudo chown -R $(whoami) /usr/local/lib/pkgconfig
 brew install tmux git git-credential-manager
+
+brew install python3
+pip3 install powerline-status
+Bundle 'Lokaltog/powerline', {'rtp': 'powerline/bindings/vim/'}
+
 git config --global user.email "jay.ryan@ge.com"
 git config --global user.name "Jay Ryan"
