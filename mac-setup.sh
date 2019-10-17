524954b11cc4e8fa1845f1e7d536181e1fa1114e
Jay Ryan <jay.ryan@ge.com>
2019-10-17 09:00:04 -0400

    update bash profile and add tmux config

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,6 +1,6 @@
 brew cask install iterm2 visual-studio-code git spectacle slack docker powershell java
 sudo chown -R $(whoami) /usr/local/lib/pkgconfig
-brew install tmux git git-credential-manager
+brew install tmux git git-credential-manager pip awscli bash-completion htop openssl
 
 brew install python3
 pip3 install powerline-status
